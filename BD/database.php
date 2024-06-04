af7132e22780ae2b20a38e7523a978b8ec846125
Jonathan Montalvo <jmp20245@gmail.com>
2024-06-04 08:21:05 -0600

    Se acabo el database, con la conexion bien

--- BD/database.php
+++ BD/database.php
@@ -27,7 +27,7 @@ class Database
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ];
-        $dns = "mysql:host=localhost;dbname=Progweb";
+        $dns = "mysql:host=localhost;dbname=venta_videojuegos";
         $user = "root";
         $password = "";
         $this->connection = new PDO($dns, $user, $password, $options);
