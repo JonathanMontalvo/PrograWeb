472968f6dad0bf91606af9caea9beafc5047df76
unknown <mtrljesus@gmail.com>
2024-06-15 14:15:43 -0600

    Agrego la pagina Home

--- pagina/index.php
+++ pagina/index.php
@@ -1,2 +1,38 @@
 <?php
+    include('../layout/Nav.php');
 ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Diseño Web</title>
+    <link rel="stylesheet" href="../CSS/style.css">
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Share+Tech&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+
+</head>
+<body>  
+
+    <div class="container">
+        <header>
+            <h1 class="tii">BIENVENIDO <i class="fas fa-robot" style="font-size: 60px; position: relative; top: -13px;" ></i></h1>
+            <h2>¡Lo último en entretenimiento!</h2>
+            <a href="Login.php">
+                <br>
+            <button type="button">
+                Compra ahora!!
+                <img src="icono/bxs-right-arrow-alt.svg" alt="">
+            </button></a>
+        </header>
+        <img class="emp" src="../IMG/EMP.png" alt="">
+        <img class="fondo-emp" src="../IMG/pattern..png" alt="">
+    </div>
+</body>
+</html>
